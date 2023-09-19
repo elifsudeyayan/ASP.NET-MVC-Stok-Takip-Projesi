0ef885c7f478826a7f9eef09f7f0703431d1ac1b
Elif Sude Yayan <elifsuyn00@gmail.com>
2023-09-20 02:25:11 +0300

    Add project files.

---
+++ ESyMVCApp.Web/Controllers/OrnekController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ESyMVCApp.Web.Controllers
+{
+
+    public class Product2
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class OrnekController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var productList = new List<Product2>()
+            {
+                new(){ Id=1, Name="Kalem" },
+                new(){ Id=2, Name="Defter" },
+                new(){ Id=2, Name="Silgi" },
+            };
+        
+
+            return View(productList);
+        }
+
+        
+
+
+
+
+        public IActionResult Index2()
+        {
+            // veritabanı kaydetme işlemi buradan yapılır
+            return RedirectToAction("Index", "Ornek");
+        }
+
+        public IActionResult ParametreView(int id)
+        {
+            return RedirectToAction("JsonResultParametre", "Ornek", new { id = id });
+        }
+
+        public IActionResult JsonResultParametre(int id)
+        {
+            return Json(new { Id = id });
+        }
+    }
+}
+
---
+++ ESyMVCApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,76 @@
+﻿using ESyMVCApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESyMVCApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    { 
+        private AppDbContext _context;
+
+        private readonly ProductRepository _productRepository;
+        public ProductsController(AppDbContext context)
+        {
+            _productRepository = new ProductRepository();
+
+            //DI container
+            //Dependency Injection Pattern
+
+            _context = context;
+            //Linq Method
+
+      
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+        public IActionResult Remove(int id)
+        {
+            var product = _context.Products.Find(id);
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Add(Product newProduct)
+        {
+            _context.Products.Add(newProduct);
+            _context.SaveChanges();
+
+
+            TempData["status"] = "Ürün başarıyla eklendi.";
+            return RedirectToAction("Index"); 
+        
+        }
+        [HttpGet]
+        public IActionResult  Update(int id)
+        {
+
+            var product = _context.Products.Find(id);
+
+            return View(product);
+        }
+
+        [HttpPost]
+
+        public IActionResult Update(Product updateProduct)
+        {
+
+            _context.Products.Update(updateProduct);
+            _context.SaveChanges();
+
+            TempData["status"] = "Ürün başarıyla güncellendi.";
+            return RedirectToAction("Index");
+        }
+    }
+}
